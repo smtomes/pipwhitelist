4c5bd7954b36580f4730f7d98db9e1097a9722e7
smtomes <smtomes@gmail.com>
2022-10-09 16:22:59 -0500

    updates

---
+++ src/main/java/net/azurewebsites/pipwhitelist/pipwhitelist/PipwhitelistApplication.java
@@ -0,0 +1,13 @@
+package net.azurewebsites.pipwhitelist.pipwhitelist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipwhitelistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipwhitelistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/azurewebsites/pipwhitelist/pipwhitelist/PipwhitelistApplicationTests.java
@@ -0,0 +1,13 @@
+package net.azurewebsites.pipwhitelist.pipwhitelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipwhitelistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
